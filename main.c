00c1e085a6b3c674bade9d797ccf42e005d005ae
Thomas Maloney <ThomasTNF@live.com>
2019-10-30 00:05:48 -0500

    kinda added get value from opcode but not really

--- main.c
+++ main.c
@@ -1,13 +1,36 @@
 #include <stdio.h>
 #include <stdint.h>
 #include "io.h"
+#include "tree.h"
+#include <stdlib.h>
+void doshit(uint16_t *num1, uint8_t *num2) {
+    printf("%d, %d\n",*num1,*num2);
+}
+
 
 int main(int argc, char *argv[]) {
-  if (argc > 1) {
+  /*if (argc > 1) {
     uint8_t* bytes = get_bytes_from_file(argv[1]);
     printf("%d, %d\n", bytes[0], bytes[1]);
   } else {
     printf("No file was given.\n");
-  }
+  }*/
+  
+  uint16_t a = (1 << 9) + 1;
+  uint8_t b = 120;
+  uint16_t *pA = &a;
+  uint8_t *pB = &b;
+  /*doshit(pA, pB);*/
+
+  /*void (*doshit_ptr)(uint16_t*, uint8_t*) = &doshit;*/
+  tree_t *t = malloc(sizeof(tree_t)); 
+  tree_init(t);
+  char* bs = "101";
+  uint16_t opcode = 5 << 8;
+  tree_node_t *leaf = tree_insert(t,bs,doshit);
+  (*leaf->fun_ptr)(pA,pB);
+  void (*doshit_ptr)(uint16_t*,uint8_t*) = get_value(t,bs);
+  (*doshit_ptr)(pA,pB);
+
   return 0;
 }
--- tree.h
+++ tree.h
@@ -14,7 +14,9 @@ typedef struct tree {
 } tree_t;
 
 void tree_init(tree_t* t);
-tree_node_t* tree_insert(tree_t *t, char* bs, void (*fun_ptr)(uint16_t*, uint8_t*));
+tree_node_t* tree_insert(tree_t *t, char *bs, void (*fun_ptr)(uint16_t*, uint8_t*));
 void (*get_value(tree_t *t, char *bs))(uint16_t*, uint8_t*);
+/*Made for passing numbers instead of bit strings*/
+void (*get_value_op(tree_t *t, uint8_t *opcode))(uint16_t*, uint8_t*);
 
 #endif
