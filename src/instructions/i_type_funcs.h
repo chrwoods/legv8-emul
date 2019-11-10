2b7cf65da42e47781106c715157ead0857782d6d
Christopher Woods <chrisscottwoods@gmail.com>
2019-11-10 16:49:31 -0600

    Changed immediate to signed, added converting to signed helper, added B+CB type params

--- src/instructions/i_type_funcs.h
+++ src/instructions/i_type_funcs.h
@@ -6,56 +6,56 @@
 
 void instruction_addi(uint16_t* registers, uint8_t* instruction) {
   uint8_t rn, rd;
-  uint16_t immediate;
+  int16_t immediate;
   get_i_format_params(instruction, &immediate, &rn, &rd);
   printf("ADDI called: X%d = X%d + #%d.\n", rd, rn, immediate);
 }
 
 void instruction_addis(uint16_t* registers, uint8_t* instruction) {
   uint8_t rn, rd;
-  uint16_t immediate;
+  int16_t immediate;
   get_i_format_params(instruction, &immediate, &rn, &rd);
   printf("ADDIS called: X%d = X%d + #%d.\n", rd, rn, immediate);
 }
 
 void instruction_subi(uint16_t* registers, uint8_t* instruction) {
   uint8_t rn, rd;
-  uint16_t immediate;
+  int16_t immediate;
   get_i_format_params(instruction, &immediate, &rn, &rd);
   printf("SUBI called: X%d = X%d - #%d.\n", rd, rn, immediate);
 }
 
 void instruction_subis(uint16_t* registers, uint8_t* instruction) {
   uint8_t rn, rd;
-  uint16_t immediate;
+  int16_t immediate;
   get_i_format_params(instruction, &immediate, &rn, &rd);
   printf("SUBIS called: X%d = X%d - #%d.\n", rd, rn, immediate);
 }
 
 void instruction_andi(uint16_t* registers, uint8_t* instruction) {
   uint8_t rn, rd;
-  uint16_t immediate;
+  int16_t immediate;
   get_i_format_params(instruction, &immediate, &rn, &rd);
   printf("ANDI called: X%d = X%d & #%d.\n", rd, rn, immediate);
 }
 
 void instruction_andis(uint16_t* registers, uint8_t* instruction) {
   uint8_t rn, rd;
-  uint16_t immediate;
+  int16_t immediate;
   get_i_format_params(instruction, &immediate, &rn, &rd);
   printf("ANDIS called: X%d = X%d & #%d.\n", rd, rn, immediate);
 }
 
 void instruction_orri(uint16_t* registers, uint8_t* instruction) {
   uint8_t rn, rd;
-  uint16_t immediate;
+  int16_t immediate;
   get_i_format_params(instruction, &immediate, &rn, &rd);
   printf("ORRI called: X%d = X%d | #%d.\n", rd, rn, immediate);
 }
 
 void instruction_eori(uint16_t* registers, uint8_t* instruction) {
   uint8_t rn, rd;
-  uint16_t immediate;
+  int16_t immediate;
   get_i_format_params(instruction,&immediate, &rn, &rd);
   printf("EORI called: X%d = X%d ^ #%d.\n", rd, rn, immediate);
 }
--- src/instructions/instruction_utils.h
+++ src/instructions/instruction_utils.h
@@ -3,8 +3,8 @@
 
 #include <stdint.h>
 
-int get_param(uint8_t* instruction, short offset, short length) {
-  int temp = 0;
+int32_t get_param(uint8_t* instruction, short offset, short length) {
+  int32_t temp = 0;
   instruction += offset / 8;
   offset = 7 - (offset % 8);
   for (int i = 0; i < length; i++) {
@@ -16,6 +16,17 @@ int get_param(uint8_t* instruction, short offset, short length) {
     temp += (*instruction >> offset--) & 1;
   }
   return temp;
+}
+
+int32_t convert_to_signed(uint32_t to_convert, short length) {
+  uint32_t check = 1 << (length - 1);
+  int32_t converted = to_convert;
+  if ((to_convert & check) != 0) {
+    converted -= check;
+    converted *= -1;
+    converted += 1;
+  }
+  return converted;
 }
 
 void get_r_format_params(uint8_t* instruction, uint8_t* rm, uint8_t* shamt, uint8_t* rn, uint8_t* rd) {
@@ -32,10 +43,19 @@ void get_d_format_params(uint8_t* instruction, uint16_t* address, uint8_t* op2, 
   *rt = get_param(instruction, 27, 5);
 }
 
-void get_i_format_params(uint8_t* instruction, uint16_t* immediate, uint8_t* rn, uint8_t* rd) {
-  *immediate = get_param(instruction, 10, 12);
+void get_i_format_params(uint8_t* instruction, int16_t* immediate, uint8_t* rn, uint8_t* rd) {
+  *immediate = convert_to_signed(get_param(instruction, 10, 12), 12);
   *rn = get_param(instruction, 22, 5);
   *rd = get_param(instruction, 27, 5);
+}
+
+void get_b_format_params(uint8_t* instruction, int32_t* address) {
+  *address = convert_to_signed(get_param(instruction, 6, 26), 26);
+}
+
+void get_cb_format_params(uint8_t* instruction, int32_t* address, uint8_t* rt) {
+  *address = convert_to_signed(get_param(instruction, 8, 19), 19);
+  *rt = get_param(instruction, 27, 5);
 }
 
 #endif
