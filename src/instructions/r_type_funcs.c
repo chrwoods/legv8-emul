e039e34c1a1b98558b682be6915d5a65ff1e3ebd
Christopher Woods <chrisscottwoods@gmail.com>
2019-11-11 00:23:24 -0600

    Swapped rn and rm, implemented all R-type

--- src/instructions/r_type_funcs.c
+++ src/instructions/r_type_funcs.c
@@ -8,7 +8,7 @@ void instruction_add(emulator_t* emulator, uint8_t* instruction) {
   uint8_t rm, shamt, rn, rd;
   get_r_format_params(instruction, &rm, &shamt, &rn, &rd);
   //printf("ADD called: X%d = X%d + X%d with shamt %d.\n", rd, rn, rm, shamt);
-  int64_t result = get_reg(emulator, rm) + get_reg(emulator, rn);
+  int64_t result = get_reg(emulator, rn) + get_reg(emulator, rm);
   set_reg(emulator, rd, result, 1);
 }
 
@@ -16,7 +16,7 @@ void instruction_adds(emulator_t* emulator, uint8_t* instruction) {
   uint8_t rm, shamt, rn, rd;
   get_r_format_params(instruction, &rm, &shamt, &rn, &rd);
   //printf("ADDS called: X%d = X%d + X%d with shamt %d.\n", rd, rn, rm, shamt);
-  int64_t result = get_reg(emulator, rm) + get_reg(emulator, rn);
+  int64_t result = get_reg(emulator, rn) + get_reg(emulator, rm);
   set_condition_codes(emulator, result);
   set_reg(emulator, rd, result, 1);
 }
@@ -25,7 +25,7 @@ void instruction_sub(emulator_t* emulator, uint8_t* instruction) {
   uint8_t rm, shamt, rn, rd;
   get_r_format_params(instruction, &rm, &shamt, &rn, &rd);
   //printf("SUB called: X%d = X%d - X%d with shamt %d.\n", rd, rn, rm, shamt);
-  int64_t result = get_reg(emulator, rm) - get_reg(emulator, rn);
+  int64_t result = get_reg(emulator, rn) - get_reg(emulator, rm);
   set_reg(emulator, rd, result, 1);
 }
 
@@ -33,7 +33,7 @@ void instruction_subs(emulator_t* emulator, uint8_t* instruction) {
   uint8_t rm, shamt, rn, rd;
   get_r_format_params(instruction, &rm, &shamt, &rn, &rd);
   //printf("SUBS called: X%d = X%d - X%d with shamt %d.\n", rd, rn, rm, shamt);
-  int64_t result = get_reg(emulator, rm) - get_reg(emulator, rn);
+  int64_t result = get_reg(emulator, rn) - get_reg(emulator, rm);
   set_condition_codes(emulator, result);
   set_reg(emulator, rd, result, 1);
 }
@@ -42,7 +42,7 @@ void instruction_and(emulator_t* emulator, uint8_t* instruction) {
   uint8_t rm, shamt, rn, rd;
   get_r_format_params(instruction, &rm, &shamt, &rn, &rd);
   //printf("AND called: X%d = X%d & X%d with shamt %d.\n", rd, rn, rm, shamt);
-  int64_t result = get_reg(emulator, rm) & get_reg(emulator, rn);
+  int64_t result = get_reg(emulator, rn) & get_reg(emulator, rm);
   set_reg(emulator, rd, result, 1);
 }
 
@@ -50,7 +50,7 @@ void instruction_ands(emulator_t* emulator, uint8_t* instruction) {
   uint8_t rm, shamt, rn, rd;
   get_r_format_params(instruction, &rm, &shamt, &rn, &rd);
   //printf("ANDS called: X%d = X%d & X%d with shamt %d.\n", rd, rn, rm, shamt);
-  int64_t result = get_reg(emulator, rm) & get_reg(emulator, rn);
+  int64_t result = get_reg(emulator, rn) & get_reg(emulator, rm);
   set_condition_codes(emulator, result);
   set_reg(emulator, rd, result, 1);
 }
@@ -59,7 +59,7 @@ void instruction_orr(emulator_t* emulator, uint8_t* instruction) {
   uint8_t rm, shamt, rn, rd;
   get_r_format_params(instruction, &rm, &shamt, &rn, &rd);
   //printf("ORR called: X%d = X%d | X%d with shamt %d.\n", rd, rn, rm, shamt);
-  int64_t result = get_reg(emulator, rm) | get_reg(emulator, rn);
+  int64_t result = get_reg(emulator, rn) | get_reg(emulator, rm);
   set_reg(emulator, rd, result, 1);
 }
 
@@ -67,14 +67,15 @@ void instruction_eor(emulator_t* emulator, uint8_t* instruction) {
   uint8_t rm, shamt, rn, rd;
   get_r_format_params(instruction, &rm, &shamt, &rn, &rd);
   //printf("EOR called: X%d = X%d ^ X%d with shamt %d.\n", rd, rn, rm, shamt);
-  int64_t result = get_reg(emulator, rm) ^ get_reg(emulator, rn);
+  int64_t result = get_reg(emulator, rn) ^ get_reg(emulator, rm);
   set_reg(emulator, rd, result, 1);
 }
 
 void instruction_br(emulator_t* emulator, uint8_t* instruction) {
   uint8_t rm, shamt, rn, rd;
   get_r_format_params(instruction, &rm, &shamt, &rn, &rd);
-  
+  emulator->pc = get_reg(emulator, rn); //make sure this is Rn, book says Rt!
+  emulator->pc -= 4; //we're about to increment it by 4 in main.c, so we need to decrease it in preparation
 }
 
 // this function is a quick bodge to not count a Sheaffer-defined method in pipeline/cycle counts
@@ -111,11 +112,13 @@ void instruction_halt(emulator_t* emulator, uint8_t* instruction) {
 void instruction_mul(emulator_t* emulator, uint8_t* instruction) {
   uint8_t rm, shamt, rn, rd;
   get_r_format_params(instruction, &rm, &shamt, &rn, &rd);
-  print_register(rd, emulator->registers[rd]);
+  int64_t result = get_reg(emulator, rn) * get_reg(emulator, rm);
+  set_reg(emulator, rd, result, 1);
 }
 
 void instruction_div(emulator_t* emulator, uint8_t* instruction) {
   uint8_t rm, shamt, rn, rd;
   get_r_format_params(instruction, &rm, &shamt, &rn, &rd);
-  print_register(rd, emulator->registers[rd]);
+  int64_t result = get_reg(emulator, rn) / get_reg(emulator, rm);
+  set_reg(emulator, rd, result, 1);
 }
