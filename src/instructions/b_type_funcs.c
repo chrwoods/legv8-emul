d99b9b61a08768b48c482ea4e0a33107bf70206c
Thomas Maloney <ThomasTNF@live.com>
2019-11-11 00:41:42 -0600

    Implemented B types I think

--- src/instructions/b_type_funcs.c
+++ src/instructions/b_type_funcs.c
@@ -7,5 +7,8 @@ void instruction_b(emulator_t* emulator, uint8_t* instruction) {
 }
 
 void instruction_bl(emulator_t* emulator, uint8_t* instruction) {
-    
+    int32_t address;
+    get_b_format_params(instruction, address);
+    set_reg(emulator, 30, emulator->pc + 4, 0);
+    emulator->pc += address;
 }
