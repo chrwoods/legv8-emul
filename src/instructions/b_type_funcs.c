208652d4956383d9731fb1ecdb6090782b61f318
Thomas Maloney <ThomasTNF@live.com>
2019-11-11 00:43:04 -0600

    offset the auto incrementer for the pc

--- src/instructions/b_type_funcs.c
+++ src/instructions/b_type_funcs.c
@@ -3,12 +3,12 @@
 void instruction_b(emulator_t* emulator, uint8_t* instruction) {
   int32_t address;
   get_b_format_params(instruction, address);
-  emulator->pc += address;
+  emulator->pc += address - 4;
 }
 
 void instruction_bl(emulator_t* emulator, uint8_t* instruction) {
-    int32_t address;
-    get_b_format_params(instruction, address);
-    set_reg(emulator, 30, emulator->pc + 4, 0);
-    emulator->pc += address;
+  int32_t address;
+  get_b_format_params(instruction, address);
+  set_reg(emulator, 30, emulator->pc + 4, 0);
+  emulator->pc += address - 4;
 }
