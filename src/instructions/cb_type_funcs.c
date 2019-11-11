4f74db6b1f6829ab66d506c1e4207b2df2a5a556
Christopher Woods <chrisscottwoods@gmail.com>
2019-11-11 00:48:08 -0600

    Implemented CB-type

--- src/instructions/cb_type_funcs.c
+++ src/instructions/cb_type_funcs.c
@@ -1,13 +1,43 @@
 #include "cb_type_funcs.h"
 
-void instruction_bcond(emulator_t* emulator, uint8_t* instruction) {
+void add_control_hazard(emulator_t* emulator) {
+  emulator->control_hazards++;
+  emulator->bypass_bubbles += 3;
+  emulator->no_bypass_bubbles += 3;
+  shift_written(emulator->bypass, 3);
+  shift_written(emulator->no_bypass, 3);
+}
+  
 
+void instruction_bcond(emulator_t* emulator, uint8_t* instruction) {
+  int32_t address;
+  uint8_t rt;
+  get_cb_format_params(instruction, &address, &rt);
+  if (rt > 13) {
+    halt(emulator);
+  }
+  if (emulator->condition_codes[rt]) {
+    emulator->pc += address - 4;
+    add_control_hazard(emulator);
+  }
 }
 
 void instruction_cbnz(emulator_t* emulator, uint8_t* instruction) {
-
+  int32_t address;
+  uint8_t rt;
+  get_cb_format_params(instruction, &address, &rt);
+  if (get_reg(emulator, rt)) {
+    emulator->pc += address - 4;
+    add_control_hazard(emulator);
+  }
 }
 
 void instruction_cbz(emulator_t* emulator, uint8_t* instruction) {
-
+  int32_t address;
+  uint8_t rt;
+  get_cb_format_params(instruction, &address, &rt);
+  if (!get_reg(emulator, rt)) {
+    emulator->pc += address - 4;
+    add_control_hazard(emulator);
+  }
 }
